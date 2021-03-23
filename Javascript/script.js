9e9a54229c9dc2307ba9fecb047e9c8c8e79e3eb
EmmanueleLibraro <emmanuelelibraro95@gmail.com>
2021-03-23 16:08:45 +0100

    .

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,5 +1,12 @@
 //Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
-for (i = 0; i < 100; i++){
-    
+var numeri = 100;
+console.log(numeri);
+
+for (var i = 0; i < 100; i++){
+    numeri[i] = i + 1;
+}
+
+for (var i = 0; i < numeri.length; i++){
+    console.log(numeri[i]);
 }
